5729ddee9ddb6433fd7abec67f6991d681f5ef53
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-09 13:59:17 +0900

    1st commit

--- Stop Watch/ViewController.swift
+++ Stop Watch/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    var count = 0
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: { (timer) in
+            self.update()
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func update() {
+        count = count + 1
+        let min = count / 60 / 100
+        let sec = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sec * 100)
+        timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
     }
-
 
 }
 
